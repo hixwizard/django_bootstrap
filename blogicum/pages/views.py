76e74dd5e6bd97acfbd1458d5f3c2472fd5a38ab
Стас Баринов <stanislavbarinov0@gmail.com>
2024-03-18 14:59:07 +0300

    test5

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-
+from django.shortcuts import render
 
 class TemplateAboutView(TemplateView):
     template_name = 'pages/about.html'
@@ -7,3 +7,15 @@ class TemplateAboutView(TemplateView):
 
 class TemplateRulesView(TemplateView):
     template_name = 'pages/rules.html'
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def server_error(request):
+    return render(request, 'pages/500.html', status=500)
