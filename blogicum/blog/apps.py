55e1d9d2de36d00530fbc491a34e61a519e58a8f
Стас Баринов <stanislavbarinov0@gmail.com>
2024-03-26 15:41:19 +0300

    output.txt delete

--- blogicum/blog/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogConfig(AppConfig):
-    """Конфигурация приложения Блог."""
-
-    default_auto_field: str = 'django.db.models.BigAutoField'
-    name: str = 'blog'
-    verbose_name: str = 'Блог'
